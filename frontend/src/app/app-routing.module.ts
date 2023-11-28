fea9cd1a3dc5fa9aa3cacf6e11dc49a9dd175722
henriqtorresl <henriquetlandin@gmail.com>
2023-11-27 21:42:04 -0300

    ajustes na estilização geral e modularização

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { PaginaConsultaComponent } from "./modules/pagina-consulta/pagina-consulta.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "consulta" },
+  { path: 'consulta', component: PaginaConsultaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
