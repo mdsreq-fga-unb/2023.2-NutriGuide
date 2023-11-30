dbe273269081126515a43261d2d32c25db0dd5b8
henriqtorresl <henriquetlandin@gmail.com>
2023-11-30 13:50:03 -0300

    adicionando logica para pegar a rota atual

--- frontend/src/app/modules/header/header.component.ts
+++ frontend/src/app/modules/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  private rotaAtiva: string = '';
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.pegarRotaAtual();
+
+    console.log(this.rotaAtiva);
+  }
+
+  pegarRotaAtual(): void {
+    this.rotaAtiva = String(this.router.config[1].path);
+  }
 
 }
