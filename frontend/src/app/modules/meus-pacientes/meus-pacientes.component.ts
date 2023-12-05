36ff41f2c8e6bc5cdea934aa57c4bd4541ebc043
henriqtorresl <henriquetlandin@gmail.com>
2023-12-05 16:49:24 -0300

    adicionando a tela de consultar pacientes

--- frontend/src/app/modules/meus-pacientes/meus-pacientes.component.ts
+++ frontend/src/app/modules/meus-pacientes/meus-pacientes.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// apagar depois:
+interface Paciente {
+  nome: string;
+  idade: number;
+  contato: number;
+  condicoesMedicas: string;
+}
 
 @Component({
   selector: 'app-meus-pacientes',
   templateUrl: './meus-pacientes.component.html',
   styleUrls: ['./meus-pacientes.component.scss']
 })
-export class MeusPacientesComponent {
+export class MeusPacientesComponent implements OnInit {
+
+  // apagar depois
+  colunas: string[] = ['Paciente', 'Idade', 'Contato', 'Condições Médicas Principais', 'Ações'];
+
+  // apagar depois
+  paciente1: Paciente = {
+    nome: 'Henrique',
+    idade: 21,
+    contato: 984441480,
+    condicoesMedicas: 'sei la sei la sei la'
+  }
+
+  // apagar depois
+  linhas: Paciente[] = [
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1,
+    this.paciente1
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
