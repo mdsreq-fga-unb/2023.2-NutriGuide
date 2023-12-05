3c7e3d13bed785b7fd4f536dd864213295d72fc5
henriqtorresl <henriquetlandin@gmail.com>
2023-12-05 19:01:51 -0300

    adicionando o formulario de cadastrar paciente

--- frontend/src/app/modules/cadastrar-paciente/cadastrar-paciente.component.ts
+++ frontend/src/app/modules/cadastrar-paciente/cadastrar-paciente.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-cadastrar-paciente',
   templateUrl: './cadastrar-paciente.component.html',
   styleUrls: ['./cadastrar-paciente.component.scss']
 })
-export class CadastrarPacienteComponent {
+export class CadastrarPacienteComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<CadastrarPacienteComponent>
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  cancelar(): void {
+    this.dialogRef.close();
+  }
+
+  registrar(): void {
+    
+  }
 
 }
