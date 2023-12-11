0f42fbd5e17a87a3e56db85285f850cdacb626bc
henriqtorresl <henriquetlandin@gmail.com>
2023-12-11 00:35:32 -0300

    modulo de download por pdf finalizado

--- frontend/src/app/modules/consultar-plano/consultar-plano.component.ts
+++ frontend/src/app/modules/consultar-plano/consultar-plano.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import AlimentoPlanoAlimentar from 'src/app/interfaces/AlimentoPlanoAlimentar';
 import UsuarioPaciente from 'src/app/interfaces/UsuarioPaciente';
 import { AlimentoService } from 'src/app/services/alimento-service/alimento.service';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-consultar-plano',
@@ -45,6 +46,36 @@ export class ConsultarPlanoComponent implements OnInit {
 
   fechar(): void {
     this.dialogRef.close('fechar');
+  }
+
+  downloadPdf(): void {
+    const pdf = new jsPDF('p', 'pt', 'a4');
+
+    pdf.text('Plano Alimentar', 210, 30, { align: 'center' });
+    pdf.text(`Paciente: ${this.paciente.nome_usuario}`, 210, 50, { align: 'center' });
+    pdf.text(
+      '-----------------------------------------------------------------------------------------------------------------------------------------------------------',
+      210, 70, { align: 'center' }
+    );
+
+    let yPos = 100;
+
+    this.alimentosPlanoAlimentarList.forEach((a) => {
+      pdf.text(`Refeição: ${a.nome_refeicao}`, 20, yPos, { align: 'left' });
+      pdf.text(`Alimento: ${a.nome_alimento}, ${a.quantidade_grama} gramas`, 20, yPos + 20, { align: 'left' });
+      pdf.text(`Quantidade de proteínas: ${a.qnt_proteina}`, 20, yPos + 40, { align: 'left' });
+      pdf.text(`Quantidade de carboidratos: ${a.qnt_carboidrato}`, 20, yPos + 60, { align: 'left' });
+      pdf.text(`Quantidade de gorduras: ${a.qnt_gordura}`, 20, yPos + 80, { align: 'left' });
+
+      yPos += 100;
+
+      pdf.text('-----------------------------------', 210, yPos, { align: 'left' });
+      yPos += 40;
+    });
+
+    pdf.save(`PlanoAlimentar_${this.paciente.nome_usuario}.pdf`);
+
+    this.snackbar.open('PDF baixado com sucesso!', 'OK', {duration: 2500});
   }
 
 }
--- frontend/src/app/modules/meu-plano-alimentar/meu-plano-alimentar.component.ts
+++ frontend/src/app/modules/meu-plano-alimentar/meu-plano-alimentar.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import AlimentoPlanoAlimentar from 'src/app/interfaces/AlimentoPlanoAlimentar';
 import UsuarioPaciente from 'src/app/interfaces/UsuarioPaciente';
 import { AlimentoService } from 'src/app/services/alimento-service/alimento.service';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-meu-plano-alimentar',
@@ -21,7 +22,7 @@ export class MeuPlanoAlimentarComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public paciente: UsuarioPaciente,
     private alimentoService: AlimentoService,
     private snackbar: MatSnackBar
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.buscarAlimentosPlanoAlimentar();
@@ -36,15 +37,47 @@ export class MeuPlanoAlimentarComponent implements OnInit {
 
       this.load = true;
     },
-    (err) => {
-      this.load = true;
+      (err) => {
+        this.load = true;
 
-      this.snackbar.open('Você não possui nenhum Plano Alimentar!', 'OK', {duration: 3000});
-    });
+        this.snackbar.open('Você não possui nenhum Plano Alimentar!', 'OK', { duration: 3000 });
+      });
   }
 
   fechar(): void {
     this.dialogRef.close('fechar');
+  }
+
+  downloadPdf(): void {
+    const pdf = new jsPDF('p', 'pt', 'a4');
+
+    pdf.text('Plano Alimentar', 210, 30, { align: 'center' });
+    pdf.text(`Paciente: ${this.paciente.nome_usuario}`, 210, 50, { align: 'center' });
+    pdf.text(
+      '-----------------------------------------------------------------------------------------------------------------------------------------------------------',
+      210, 70, { align: 'center' }
+    );
+
+    let yPos = 100;
+
+    this.alimentosPlanoAlimentarList.forEach((a) => {
+      pdf.text(`Refeição: ${a.nome_refeicao}`, 20, yPos, { align: 'left' });
+      pdf.text(`Alimento: ${a.nome_alimento}, ${a.quantidade_grama} gramas`, 20, yPos + 20, { align: 'left' });
+      pdf.text(`Quantidade de proteínas: ${a.qnt_proteina}`, 20, yPos + 40, { align: 'left' });
+      pdf.text(`Quantidade de carboidratos: ${a.qnt_carboidrato}`, 20, yPos + 60, { align: 'left' });
+      pdf.text(`Quantidade de gorduras: ${a.qnt_gordura}`, 20, yPos + 80, { align: 'left' });
+
+      yPos += 100;
+
+      pdf.text('-----------------------------------', 210, yPos, { align: 'left' });
+      yPos += 40;
+    });
+
+    pdf.text("Para mais informações, entre em contato com seu nutricionista!", 60, yPos + 40);
+
+    pdf.save(`PlanoAlimentar_${this.paciente.nome_usuario}.pdf`);
+
+    this.snackbar.open('PDF baixado com sucesso!', 'OK', {duration: 2500});
   }
 
 }
